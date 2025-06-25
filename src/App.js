a73184e60ade068cbad770bc68f408f5be18d86d
25Mishka <86193316+25Mishka@users.noreply.github.com>
2025-06-25 19:54:45 +0200

    added bootstrap react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by {""}
-        <a href="">Mosa Mokebe</a> and is
-        <a
-          href="https://github.com/25Mishka/Weather-react-app-upgraded"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by {""}
+          <a href="">Mosa Mokebe</a> and is
+          <a
+            href="https://github.com/25Mishka/Weather-react-app-upgraded"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
