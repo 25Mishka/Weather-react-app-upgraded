ef6f5d5f456fc7139973e1616c47e70d6f1cb23d
25Mishka <86193316+25Mishka@users.noreply.github.com>
2025-06-26 16:13:40 +0200

    added new file (weather)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <h1> Weather App </h1>
+        <Weather />
         <footer>
           This project was coded by {""}
           <a href="https://github.com/25Mishka/Weather-react-app-upgraded">
