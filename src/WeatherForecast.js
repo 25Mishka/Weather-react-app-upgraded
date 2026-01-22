fb6e52f1fb5b42224d25dfac10abdd95bbf7cfb2
25Mishka <86193316+25Mishka@users.noreply.github.com>
2026-01-22 10:47:25 +0200

    update forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import Axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
   function handleResponse(response) {
     console.log(response.data);
   }
-  console.log(props);
 
-  let longitude = props.coordinates.long;
-  let latitude = props.coordinates.lat;
-  let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  if (loaded) {
+    let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
+    let longitude = props.coordinates.long;
+    let latitude = props.coordinates.lat;
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day"> Thu</div>
-          <WeatherIcon code="broken-clouds-day" size={36} />
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
-          <div className="WeatherForecast-temperature">
-            <span className="WeatherForecast-temperature-max">19</span>
-            <span className="WeatherForecast-temperature-min">10</span>
+    axios.get(apiUrl).then(handleResponse);
+  } else {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day"> Thu</div>
+            <WeatherIcon code="broken-clouds-day" size={36} />
+
+            <div className="WeatherForecast-temperature">
+              <span className="WeatherForecast-temperature-max">19</span>
+              <span className="WeatherForecast-temperature-min">10</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
